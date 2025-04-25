916f6d4132d5ca15523356778b131d2d57d91453
Walleks Miranda <walleksmiranda@gmail.com>
2025-04-25 14:53:46 -0300

    refactor: improvement validation

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -4,46 +4,48 @@ import (
 	"errors"
 	"time"
 
+	"github.com/go-playground/validator/v10"
+	"github.com/walleksmr/golang-emailn/packages/translations"
+
 	"github.com/rs/xid"
 )
 
 type Contact struct {
-	Email string
+	Email string `validate:"email"`
 }
 
 type Campaign struct {
-	ID        string
-	Name      string
-	CreatedOn time.Time
-	Content   string
-	Contacts  []Contact
+	ID        string    `validate:"required"`
+	Name      string    `validate:"min=5,max=24"`
+	CreatedOn time.Time `validate:"required"`
+	Content   string    `validate:"min=5,max=1024"`
+	Contacts  []Contact `validate:"min=1,dive"`
 }
 
 func NewCampaign(name string, content string, emails []string) (*Campaign, error) {
-
-	if name == "" {
-		return nil, errors.New("name is required")
-	}
-
-	if content == "" {
-		return nil, errors.New("content is required")
-	}
-
-	if len(emails) == 0 {
-		return nil, errors.New("contacts is required")
-	}
 
 	contacts := make([]Contact, len(emails))
 
 	for index, email := range emails {
 		contacts[index].Email = email
 	}
-
-	return &Campaign{
+	campaign := &Campaign{
 		ID:        xid.New().String(),
 		Name:      name,
 		Content:   content,
 		CreatedOn: time.Now(),
 		Contacts:  contacts,
-	}, nil
+	}
+
+	validate := validator.New()
+
+	TranslateValidator := translations.Register(validate)
+	err := validate.Struct(campaign)
+
+	if err != nil {
+		validationErrors := err.(validator.ValidationErrors)
+		return nil, errors.New(validationErrors[0].Translate(TranslateValidator))
+	}
+
+	return campaign, nil
 }
