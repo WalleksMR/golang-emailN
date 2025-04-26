cda966cc3e7aeb8600365c2b5104231feb8951a2
Walleks Miranda <walleksmiranda@gmail.com>
2025-04-26 12:19:56 -0300

    test: Test_HandlerError_when_endpoint_returns_internal_error

---
+++ internal/endpoints/handler_error_test.go
@@ -0,0 +1,26 @@
+package endpoints
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/walleksmr/golang-emailn/internal/excptions"
+)
+
+func Test_HandlerError_when_endpoint_returns_internal_error(t *testing.T) {
+	assert := assert.New(t)
+
+	endpoint := func(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
+		return nil, 200, excptions.ErrInternal
+	}
+
+	handlerFunc := HandlerError(endpoint)
+	req, _ := http.NewRequest("GET", "/", nil)
+	res := httptest.NewRecorder()
+
+	handlerFunc.ServeHTTP(res, req)
+	assert.Equal(http.StatusInternalServerError, res.Code)
+	assert.Contains(res.Body.String(), excptions.ErrInternal.Error())
+}
