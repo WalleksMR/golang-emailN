df7b765557e3a463d6b28eae6692e4556ddbd7fa
Walleks Miranda <walleksmiranda@gmail.com>
2025-04-26 15:33:21 -0300

    test: Test_HandlerError_when_endpoint_returns_domain_error

--- internal/endpoints/handler_error_test.go
+++ internal/endpoints/handler_error_test.go
@@ -1,6 +1,7 @@
 package endpoints
 
 import (
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -23,4 +24,20 @@ func Test_HandlerError_when_endpoint_returns_internal_error(t *testing.T) {
 	handlerFunc.ServeHTTP(res, req)
 	assert.Equal(http.StatusInternalServerError, res.Code)
 	assert.Contains(res.Body.String(), excptions.ErrInternal.Error())
+}
+
+func Test_HandlerError_when_endpoint_returns_domain_error(t *testing.T) {
+	assert := assert.New(t)
+
+	endpoint := func(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
+		return nil, 200, errors.New("domain error")
+	}
+
+	handlerFunc := HandlerError(endpoint)
+	req, _ := http.NewRequest("GET", "/", nil)
+	res := httptest.NewRecorder()
+
+	handlerFunc.ServeHTTP(res, req)
+	assert.Equal(http.StatusBadRequest, res.Code)
+	assert.Contains(res.Body.String(), "domain error")
 }
